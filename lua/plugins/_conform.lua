677db00c70a532be12e1406c698d07639081c306
johan.attack <johan.attack@cgm.com>
2024-03-16 14:35:43 +0100

    Replacing formatter with conform

---
+++ lua/plugins/_conform.lua
@@ -0,0 +1,24 @@
+-- conform
+
+local M = {
+	"stevearc/conform.nvim",
+	opts = {},
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				-- Use a sub-list to run only the first available formatter
+				["_"] = { { "prettierd", "prettier" } },
+			},
+		})
+
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function(args)
+				require("conform").format({ bufnr = args.buf })
+			end,
+		})
+	end,
+}
+
+return { M }
